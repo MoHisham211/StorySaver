c54713d92b32a1bd8963cb72a6f8001bb07db116
MoHisham211 <62261376+MoHisham211@users.noreply.github.com>
2021-07-25 19:29:40 +0200

    StorySaver

---
+++ app/src/main/java/mo/zain/storysaver/ui/FilesDownloaderActivity.java
@@ -0,0 +1,199 @@
+package mo.zain.storysaver.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.FocusFinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Arrays;
+import java.util.List;
+
+import at.huber.youtubeExtractor.VideoMeta;
+import at.huber.youtubeExtractor.YouTubeExtractor;
+import at.huber.youtubeExtractor.YouTubeUriExtractor;
+import at.huber.youtubeExtractor.YtFile;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import mo.zain.storysaver.R;
+@SuppressLint("StaticFieldLeak")
+
+public class FilesDownloaderActivity extends AppCompatActivity {
+
+    @BindView(R.id.back) ImageView imageView;
+    @BindView(R.id.downloadBtn) Button button;
+    @BindView(R.id.UrlID) TextInputLayout textInputLayout;
+    //@BindView(R.id.videoView) VideoView videoView;
+    private static String newLink;
+    private Handler handler=new Handler();
+    private RewardedAd mRewardedAd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files_downloader);
+        ButterKnife.bind(this);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        mRewardedAd = null;
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                        mRewardedAd = rewardedAd;
+                    }
+                });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRewardedAd != null) {
+                    Activity activityContext = FilesDownloaderActivity.this;
+                    mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                        @Override
+                        public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                            int rewardAmount = rewardItem.getAmount();
+                            String rewardType = rewardItem.getType();
+                        }
+                    });
+                    mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                        @Override
+                        public void onAdShowedFullScreenContent() {
+                            mRewardedAd = null;
+                        }
+
+                        @Override
+                        public void onAdFailedToShowFullScreenContent(AdError adError) {
+                            finish();
+                        }
+                        @Override
+                        public void onAdDismissedFullScreenContent() {
+                            finish();
+                        }
+                    });
+
+                } else {
+                    finish();
+                }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textInputLayout.getEditText()
+                        .getText().toString().equals(""))
+                {
+                    textInputLayout.setErrorEnabled(true);
+                    textInputLayout.setError("Enter Your Link");
+                    return;
+                }else {
+                    String Value=textInputLayout.getEditText().getText().toString().trim();
+                    startDownload(Value);
+                }
+            }
+        });
+    }
+
+
+    private void startDownload(String url) {
+         new YouTubeExtractor(FilesDownloaderActivity.this) {
+            @Override
+            public void onExtractionComplete(SparseArray<YtFile> ytFiles, VideoMeta vMeta) {
+
+                if (ytFiles != null) {
+                    int itag = 18;
+                    String downloadUrl = ytFiles.get(itag).getUrl();
+                    DownloadManager.Request request = new DownloadManager
+                            .Request(Uri.parse(downloadUrl));
+
+                    request.setTitle("Video File");
+                    request.setDescription("Downloading");
+                    request.setDestinationInExternalFilesDir(FilesDownloaderActivity.this
+                            , Environment.DIRECTORY_DOWNLOADS, System.currentTimeMillis()+ ".mp4");
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setVisibleInDownloadsUi(false);
+                    DownloadManager manager = (DownloadManager)
+                            getSystemService(Context.DOWNLOAD_SERVICE);
+                    request.allowScanningByMediaScanner();
+                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI
+                            | DownloadManager.Request.NETWORK_MOBILE);
+                    if (manager != null) {
+                        manager.enqueue(request);
+                    }
+                }
+            }
+        }.extract(url, true, true);
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (mRewardedAd != null) {
+            Activity activityContext = FilesDownloaderActivity.this;
+            mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                    // Handle the reward.
+                    //Log.d(TAG, "The user earned the reward.");
+                    int rewardAmount = rewardItem.getAmount();
+                    String rewardType = rewardItem.getType();
+                }
+            });
+            mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                @Override
+                public void onAdShowedFullScreenContent() {
+                    // Called when ad is shown.
+                    //Log.d(TAG, "Ad was shown.");
+                    mRewardedAd = null;
+                }
+
+                @Override
+                public void onAdFailedToShowFullScreenContent(AdError adError) {
+                    // Called when ad fails to show.
+                    //Log.d(TAG, "Ad failed to show.");
+                    finish();
+                }
+                //-----------TMAM
+                @Override
+                public void onAdDismissedFullScreenContent() {
+                    // Called when ad is dismissed.
+                    // Don't forget to set the ad reference to null so you
+                    // don't show the ad a second time.
+                    //Log.d(TAG, "Ad was dismissed.");
+                    finish();
+                }
+            });
+
+        } else {
+            // Log.d(TAG, "The rewarded ad wasn't ready yet.");
+            super.onBackPressed();
+        }
+
+    }
+}
