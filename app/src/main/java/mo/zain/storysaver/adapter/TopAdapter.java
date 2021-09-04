46685e935763e348cf21029406e8a48850e55bfd
MoHisham211 <62261376+MoHisham211@users.noreply.github.com>
2021-09-04 22:05:45 +0200

    1.5

--- app/src/main/java/mo/zain/storysaver/adapter/TopAdapter.java
+++ app/src/main/java/mo/zain/storysaver/adapter/TopAdapter.java
@@ -2,6 +2,7 @@ package mo.zain.storysaver.adapter;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -13,13 +14,20 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.List;
 
@@ -51,7 +59,21 @@ public class TopAdapter extends RecyclerView.Adapter<TopAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull TopAdapter.ViewHolder holder, int position) {
         TopModel topModel=topModels.get(position);
-        Glide.with(context).asBitmap().load(topModel.getVedio()).into(holder.imageView);
+        holder.progressBar.setVisibility(View.VISIBLE);
+        Glide.with(context).asBitmap().load(topModel.getVedio())
+                .listener(new RequestListener<Bitmap>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                }).into(holder.imageView);
         holder.textView.setText(topModel.getName());
 
         LayoutInflater inflater = LayoutInflater.from(context);
@@ -70,6 +92,7 @@ public class TopAdapter extends RecyclerView.Adapter<TopAdapter.ViewHolder> {
             alertDg.setView(view1);
 
             final VideoView videoView = view1.findViewById(R.id.video_full);
+            final ProgressBar progressBar=view1.findViewById(R.id.proBBar);
 
             final MediaController mediaController = new MediaController(context, false);
 
@@ -77,6 +100,7 @@ public class TopAdapter extends RecyclerView.Adapter<TopAdapter.ViewHolder> {
 
                 mp.start();
                 mediaController.show(0);
+                progressBar.setVisibility(View.GONE);
                 mp.setLooping(true);
             });
 
@@ -119,10 +143,13 @@ public class TopAdapter extends RecyclerView.Adapter<TopAdapter.ViewHolder> {
         TextView textView;
         @BindView(R.id.imageButton)
         ImageButton imageButton;
+        @BindView(R.id.proBar)
+        ProgressBar progressBar;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
+
             imageButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
