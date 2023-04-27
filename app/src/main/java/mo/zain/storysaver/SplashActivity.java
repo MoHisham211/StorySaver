8de7cc2b1efb9d89bcc65ec4f20f0a01d52aa037
Eng_Mo7amed <mohamedhisham714@gmail.com>
2023-04-27 18:57:20 +0200

    2.0

--- app/src/main/java/mo/zain/storysaver/SplashActivity.java
+++ app/src/main/java/mo/zain/storysaver/SplashActivity.java
@@ -1,5 +1,7 @@
 package mo.zain.storysaver;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,89 +10,146 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.Settings;
+import android.os.storage.StorageManager;
+import android.util.Log;
 
-import com.google.android.gms.ads.AdError;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.FullScreenContentCallback;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.OnUserEarnedRewardListener;
-import com.google.android.gms.ads.rewarded.RewardItem;
 import com.google.android.gms.ads.rewarded.RewardedAd;
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 import java.util.Objects;
 
-import mo.zain.storysaver.ui.TopActivity;
-
 public class SplashActivity extends AppCompatActivity {
-    private RewardedAd mRewardedAd;
+
+    private static final int REQUEST_PERMISSIONS = 1234;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private Context context;
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+
+                    Intent data = result.getData();
+
+                    assert data != null;
+
+                    Log.d("HEY: ", data.getData().toString());
+
+                    context.getContentResolver().takePersistableUriPermission(
+                            data.getData(),
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION |
+                                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                    next();
+
+                }
+            }
+    );
+
+    private final Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //ca-app-pub-3940256099942544/5224354917
-        AdRequest adRequest = new AdRequest.Builder().build();
-        RewardedAd.load(this, "ca-app-pub-6018763248917274/8764479204",
-                adRequest, new RewardedAdLoadCallback() {
-                    @Override
-                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                        mRewardedAd = null;
-                    }
 
-                    @Override
-                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
-                        mRewardedAd = rewardedAd;
-                    }
-                });
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (mRewardedAd != null) {
-                    Activity activityContext = SplashActivity.this;
-                    mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
-                        @Override
-                        public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
-                            int rewardAmount = rewardItem.getAmount();
-                            String rewardType = rewardItem.getType();
-                        }
-                    });
-                    mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
-                        @Override
-                        public void onAdShowedFullScreenContent() {
-                            mRewardedAd = null;
-                        }
 
-                        @Override
-                        public void onAdFailedToShowFullScreenContent(AdError adError) {
-                            startActivity(new Intent(SplashActivity.this,PermessionActivity.class));
-                            finish();
-                        }
-                        @Override
-                        public void onAdDismissedFullScreenContent() {
-                            startActivity(new Intent(SplashActivity.this,PermessionActivity.class));
-                            finish();
-                        }
-                    });
+        context = getApplicationContext();
 
-                } else {
-                    startActivity(new Intent(SplashActivity.this,PermessionActivity.class));
-                    finish();
-                }
+        if (!arePermissionDenied()) {
+            next();
+            return;
+        }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionDenied()) {
+
+            // If Android 10+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                requestPermissionQ();
+                return;
             }
-        },1800);
+
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+        }
+
     }
 
     @Override
-    public void onBackPressed() {
-//        super.onBackPressed();
+    protected void onResume() {
+        super.onResume();
+        if (!arePermissionDenied()) {
+            next();
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void requestPermissionQ() {
+        StorageManager sm = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+
+        Intent intent = sm.getPrimaryStorageVolume().createOpenDocumentTreeIntent();
+        String startDir = "Android%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2F.Statuses";
+
+        Uri uri = intent.getParcelableExtra("android.provider.extra.INITIAL_URI");
+
+        String scheme = uri.toString();
+        scheme = scheme.replace("/root/", "/document/");
+        scheme += "%3A" + startDir;
+
+        uri = Uri.parse(scheme);
+
+        Log.d("URI", uri.toString());
+
+        intent.putExtra("android.provider.extra.INITIAL_URI", uri);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+
+
+        activityResultLauncher.launch(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS && grantResults.length > 0) {
+            if (arePermissionDenied()) {
+                // Clear Data of Application, So that it can request for permissions again
+                ((ActivityManager) Objects.requireNonNull(this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+                recreate();
+            } else {
+                next();
+            }
+        }
+    }
+
+    private void next() {
+
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, PermessionActivity.class));
+            finish();
+        }, 1800);
+
+    }
+
+    private boolean arePermissionDenied() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return getContentResolver().getPersistedUriPermissions().size() <= 0;
+        }
+
+        for (String permissions : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), permissions) != PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }
+        }
+        return false;
     }
 }
